711d7c7233c1a290d4a27b7a1dae48b3e901e81f
Andrei Solntsev <andrei.solntsev@gmail.com>
2022-04-27 00:56:25 +0300

    #1780 add check `$.shouldHave(partialText)` for checking a substring
    + fix existing integration tests that assumed that `$.shouldHave(text)` checks a substring

--- statics/src/test/java/integration/IntegrationTest.java
+++ statics/src/test/java/integration/IntegrationTest.java
@@ -24,6 +24,7 @@ import static com.codeborne.selenide.FileDownloadMode.HTTPGET;
 import static com.codeborne.selenide.FileDownloadMode.PROXY;
 import static com.codeborne.selenide.Selenide.executeJavaScript;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.TextCheck.FULL_TEXT;
 import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 import static com.codeborne.selenide.WebDriverRunner.hasWebDriverStarted;
 import static com.codeborne.selenide.WebDriverRunner.isIE;
@@ -79,6 +80,7 @@ public abstract class IntegrationTest extends BaseIntegrationTest {
     Configuration.proxyPort = 0;
     Configuration.proxyHost = "";
     Configuration.fileDownload = HTTPGET;
+    Configuration.textCheck = FULL_TEXT;
   }
 
   protected void openFile(String fileName) {
